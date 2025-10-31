c88cd00767cd99ffd18b319bd97241f88cfa3f00
ViktorSvertoka <victor.svertoka@gmail.com>
2025-10-31 14:34:41 +0000

    feat(ts): add data

---
+++ app/data/data.ts
@@ -0,0 +1,58 @@
+export const itemData = [
+  {
+    img: 'cat-01.jfif',
+    title: 'Shyster',
+  },
+  {
+    img: 'cat-02.jfif',
+    title: 'Lazy',
+  },
+  {
+    img: 'cat-03.jpg',
+    title: 'Brandy',
+  },
+  {
+    img: 'cat-04.jfif',
+    title: 'Milk',
+  },
+  {
+    img: 'cat-05.jpg',
+    title: 'Blinds',
+  },
+  {
+    img: 'cat-06.jpg',
+    title: 'Charles',
+  },
+  {
+    img: 'cat-07.jpg',
+    title: 'Laptop',
+  },
+  {
+    img: 'cat-08.jpg',
+    title: 'Dorothy',
+  },
+  {
+    img: 'cat-09.jpg',
+    title: 'Feather',
+  },
+  {
+    img: 'cat-10.jpg',
+    title: 'Storm',
+  },
+  {
+    img: 'cat-11.jpg',
+    title: 'Candy',
+  },
+  {
+    img: 'cat-12.jpg',
+    title: 'Sporty',
+  },
+  {
+    img: 'cat-13.jpg',
+    title: 'Fluffy',
+  },
+  {
+    img: 'cat-14.jpg',
+    title: 'Misty',
+  },
+];
